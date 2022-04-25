e55af4825d2e0498c7acdb569f29c600e5a2759e
Yuliia M <myuliia2018@gmail.com>
2022-04-25 12:47:15 -0500

    adding js link to html. adding vat  = moment to js file, creating function and localStorage

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var today = moment();
+
+
+$(document).ready(function(){
+    $("#currentDay").text(moment().format("dddd, MMMM Do")); 
+    $(".saveBtn").on("click", function(){
+        var plannerText = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, plannerText);
+
+    }); 
+    
+    $("#7 .description").val(localStorage.getItem("7"));
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
+    $("#19 .description").val(localStorage.getItem("19"));
+    $("#20 .description").val(localStorage.getItem("20"));
+
+});
